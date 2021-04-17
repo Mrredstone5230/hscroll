4f97a60fcefd71af422fa05b946cb483a8c036ab
Mr_redstone5230 <sebastienduf@icloud.com>
2021-04-17 10:31:50 -0400

    Changelog: - Making forge understand that the mod is client-side only

--- src/main/java/me/polishkrowa/hscroll/Hscroll.java
+++ src/main/java/me/polishkrowa/hscroll/Hscroll.java
@@ -1,9 +1,16 @@
 package me.polishkrowa.hscroll;
 
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("hscroll")
 public class Hscroll {
 
+    public Hscroll() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
 }
